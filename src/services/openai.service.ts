da69fda523c8947ad5c832b085b84202751abde5
gokaydmrl <gokaydemirel96@gmail.com>
2025-05-12 14:13:06 +0300

    prompt fix

--- src/services/openai.service.ts
+++ src/services/openai.service.ts
@@ -33,7 +33,7 @@ export class OpenAiService {
           type: 'response.create',
           response: {
             modalities: ['text', 'audio'],
-            instructions: `Sen mindlee tarafından geliştirilen bir yapay zeka sohbet aracısın. Kullanıcılara ismiyle seslen. Kullanıcının ismi ${username}. Pozitif, olumlu ve iyimser bir bakış açısı ile konuş. Motive edici alıntılar yap, akıl hocası ol. Amacın duyguları anlamak, kullanıcıları cesaretlendirmek, nasıl hissettiklerini öğrenmek ve onlara mentorluk etmek. Kişilere düşündürücü sorular sor. Konuşmayı aktif tutmak için pratik tavsiyeler ve çözümler hazırla. Kişinin karşılaştığı zorlukları tanımlasına yardım et ve hayallerini gerçekleştirmesine yardımcı ol. Kitaplar, makaleler veya web sitelerinden yardımcı olabilecek kaynaklar ve araçlar sağla. Siz diye hitap etme, sen diye hitap et. Bana canım deme. Cevaplarını anlaşılır, kısa ve net bir şekilde ver. Mesajlar kısa olmak zorunda. Daha önce verdiğin cevapları tekrar verme. Her seferinde nasıl yardımcı olabilirim deme, farklı yanıtlar da ver. Yapabiliyorsan özetle.`,
+            instructions: `Kullanıcılara ismiyle seslen. Kullanıcının ismi ${username}. Yalnızca neşeli bir selam ver`,
             voice: 'alloy',
             output_audio_format: 'pcm16',
             tool_choice: 'auto',
@@ -69,6 +69,12 @@ export class OpenAiService {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const parsedMessage: Record<string, any> = JSON.parse(messageData);
       this.sseService.send(parsedMessage);
+      console.log(
+        'parsedMessage',
+        parsedMessage.type === 'response.audio.delta'
+          ? parsedMessage.event_id
+          : '',
+      );
     });
 
     return this.sseService.stream$;
