057d4b889795e1ba3e85624bc5d2be2ad0fb9d92
gokaydmrl <gokaydemirel96@gmail.com>
2025-05-30 00:33:34 +0300

    fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,5 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: process.cwd() + `/.env.${process.env.NODE_ENV}` });
 
-const sql = neon(
-  'postgresql://neondb-nest_owner:npg_5fGykIgZPp0K@ep-shiny-silence-a4o56dpf-pooler.us-east-1.aws.neon.tech/neondb-nest?sslmode=require',
-);
+const sql = neon(process.env.DATABASE_URL!);
 export const client = drizzle(sql);
